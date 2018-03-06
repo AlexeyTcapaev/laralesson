7f00782ab18f08efa5ea229fc94aaa538ba445b6
AlexeyTcapaev <lexx1995@mail.ru>
2018-03-06 03:11:18 +0300

    1

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use Sluggable;
     public function category()
     {
         return $this->hasOne(Category::class);
@@ -22,5 +24,13 @@ class Post extends Model
           'post_id',
           'tag_id'
         );
+    }
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
